1754d3e3ef31ba9c6b02a78b619ebd313cf96fe4
ElvisNjogu <elvimcqueen07@gmail.com>
2022-12-19 17:40:07 -0800

    Task 9

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -11,7 +11,7 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts_half(char *str);
 void print_array(int *a, int n);
-char*_strcpy(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
 void puts2(char *str);
 int _atoi(char *s);
 #endif
